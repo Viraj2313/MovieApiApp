bb3ee5a6dbbf453d7de4099f2a9800b75892f5df
Viraj2313 <virajmm231@gmail.com>
2025-02-16 22:17:29 +0530

    search movie feature added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../assets/styles/Home.css";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { API_URL } from "../config";
 import Loader from "./Loader";
 import { triggerNotification } from "../utils/NotificationUtil";
@@ -9,7 +9,7 @@ const Home = ({ setSelectedMovie, userId, setUserId }) => {
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [movieSearch, setSearchMovie] = useState(null);
+  const [movieSearch, setSearchMovie] = useState("");
   const navigate = useNavigate();
 
   const getUserIdFromToken = async () => {
@@ -78,9 +78,12 @@ const Home = ({ setSelectedMovie, userId, setUserId }) => {
   const filteredMovies =
     movieSearch.trim() === ""
       ? movies
-      : movies.filter((movie) =>
-          movie.Title.toLowerCase().includes(movieSearch.toLowerCase())
-        );
+      : movies.filter((movie) => {
+          const searchWords = movieSearch.toLowerCase().split(" ");
+          return searchWords.every((word) =>
+            movie.Title.toLowerCase().includes(word)
+          );
+        });
   return (
     <>
       {loading ? (
@@ -88,32 +91,37 @@ const Home = ({ setSelectedMovie, userId, setUserId }) => {
       ) : (
         <>
           {error && <div className="error-message">{error}</div>}
-          <div className="flex justify-center items-center m-3 w-xs">
+          <div className="flex justify-center items-center bg-gray-800 p-4 m-4 rounded-lg shadow-lg">
             <input
               type="text"
               value={movieSearch}
               onChange={(e) => setSearchMovie(e.target.value)}
-              className="border-b border-blue-500 h-10 focus:outline-none focus:border-blue-500 w-100%"
+              className="border-b-2 border-gray-500 h-12 focus:outline-none focus:border-blue-500 text-gray-200 placeholder-gray-400 bg-gray-700 text-lg px-4 rounded-lg w-full sm:w-[380px] mr-4"
               placeholder="Enter movie to search"
             />
-            <button className="mx-4 bg-blue-600 text-white rounded px-4 py-2 focus:outline-none hover:bg-blue-700 cursor-pointer">
+            <button className="bg-blue-600 text-white rounded-lg px-6 py-3 text-lg font-semibold shadow-md hover:scale-105 transition-transform duration-300 hover:bg-blue-700 focus:outline-none">
               Search
             </button>
           </div>
+
           <ul className="movieList">
-            {filteredMovies.map((movie) => (
-              <li key={movie.imdbID} className="movie">
-                <img
-                  src={movie.Poster}
-                  alt=""
-                  onClick={() => handleClick(movie)}
-                />
-                <h3>
-                  {movie.Title}{" "}
-                  <button onClick={() => handleSave(movie)}>Save</button>
-                </h3>
-              </li>
-            ))}
+            {filteredMovies.length > 0 ? (
+              filteredMovies.map((movie) => (
+                <li key={movie.imdbID} className="movie">
+                  <img
+                    src={movie.Poster}
+                    alt=""
+                    onClick={() => handleClick(movie)}
+                  />
+                  <h3>
+                    {movie.Title}{" "}
+                    <button onClick={() => handleSave(movie)}>Save</button>
+                  </h3>
+                </li>
+              ))
+            ) : (
+              <div className="no-movies-message">No such movie found</div>
+            )}
           </ul>
         </>
       )}

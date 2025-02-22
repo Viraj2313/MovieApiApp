32009b17c23e9843a36c27eafa72821f7501447c
Viraj2313 <virajmm231@gmail.com>
2025-02-22 20:31:00 +0530

    config for reverse proxy

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,4 +1,3 @@
-export const API_URL =
-  "https://mymovieapi-h7athbbzc4fmb0es.canadacentral-01.azurewebsites.net";
+export const API_URL = "";
 // export const API_URL = "http://localhost:5132";
 // export const API_URL = "https://localhost:7264";

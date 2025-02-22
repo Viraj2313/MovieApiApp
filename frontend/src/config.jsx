9ac880b7d65a3d5b674dd2e298f11a893d1a9527
Viraj2313 <virajmm231@gmail.com>
2025-02-22 19:03:38 +0530

    proxy for api

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,4 +1,5 @@
-export const API_URL =
-  "https://mymovieapi-h7athbbzc4fmb0es.canadacentral-01.azurewebsites.net";
+// export const API_URL =
+//   "/api";
 // export const API_URL = "http://localhost:5132";
 // export const API_URL = "https://localhost:7264";
+export const API_URL = "/api";

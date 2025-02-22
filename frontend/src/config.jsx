bf82d39cf738c8a4ae811f6974009b992a0794b7
Viraj2313 <virajmm231@gmail.com>
2025-02-22 19:16:14 +0530

    PROXY debug

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,5 +1,3 @@
-// export const API_URL =
-//   "/api";
+export const API_URL = "";
 // export const API_URL = "http://localhost:5132";
 // export const API_URL = "https://localhost:7264";
-export const API_URL = "/api";

a1865195b0284634bd0dcf6f9096f779bd3cad83
Viraj2313 <virajmm231@gmail.com>
2025-02-22 16:07:52 +0530

    wishlist debug

--- frontend/src/components/WishList.jsx
+++ frontend/src/components/WishList.jsx
@@ -20,6 +20,7 @@ const WishList = () => {
       setWishlist(response.data);
       console.log(wishlist);
     } catch (error) {
+      alert("error", error.message);
       setError(error.message);
     } finally {
       setLoading(false);
@@ -72,7 +73,7 @@ const WishList = () => {
                 </li>
               ))
             ) : (
-              <p>No movies in wishlist</p>
+              <p>{error}No movies in wishlist</p>
             )}
           </ul>
         </>

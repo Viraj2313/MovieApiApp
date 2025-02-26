50483b612e304e17111d1bc57c69f254d7879557
Viraj2313 <virajmm231@gmail.com>
2025-02-26 20:41:13 +0530

    API url env config

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,4 +1,4 @@
 // export const API_URL = import.meta.LOCAL_API_URL;
 export const API_URL =
-  import.meta.env.REACT_APP_API_URL || "http://localhost:5132";
+  import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:5132";
 // export const API_URL = "https://localhost:7264";

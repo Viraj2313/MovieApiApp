b7be01b5ff850354f40a60b5d4e69854376e13fd
Viraj2313 <virajmm231@gmail.com>
2025-03-09 14:13:47 +0530

    py api url debug

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -3,5 +3,5 @@ export const API_URL =
   import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:5132";
 
 export const PY_API_URL =
-  "http://localhost:5001" || import.meta.env.VITE_PY_API_URL;
+  import.meta.env.VITE_PY_API_URL || "http://localhost:5001";
 // export const API_URL = "https://localhost:7264";

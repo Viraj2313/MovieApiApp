2d09de25c3f589aec13d4c1c723b648f27b6e648
Viraj2313 <virajmm231@gmail.com>
2025-03-11 14:40:25 +0530

    proxy set

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,4 +1,3 @@
 // export const API_URL = import.meta.LOCAL_API_URL;
-export const API_URL =
-  import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:5132";
+export const API_URL = "";
 // export const API_URL = "https://localhost:7264";
